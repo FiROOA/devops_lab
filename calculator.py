f0cf73143ef81fd6e718b96629acd18bad82a34e
FiROOA <github.com.appearsing128@passmail.net>
2026-03-31 16:55:11 +0300

    added calculator module

---
+++ calculator.py
@@ -0,0 +1,21 @@
+import logging
+
+def add_numbers(numbers):
+    """Сума списку чисел"""
+    result = sum(numbers)
+    logging.info(f"Adding {numbers} = {result}")
+    return result
+
+def multiply_numbers(numbers):
+    """Добуток списку чисел"""
+    result = 1
+    for num in numbers:
+        result *= num
+    logging.info(f"Multiplying {numbers} = {result}")
+    return result
+
+def get_max(numbers):
+    """Максимальне число зі списку"""
+    result = max(numbers)
+    logging.info(f"Max of {numbers} = {result}")
+    return result
